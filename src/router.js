011acf894c1a26d88c3e89291a36d236a5848bb8
Alec Hannaford <alec322@gmail.com>
2023-01-17 15:36:00 -0800

    added show and index into router

--- src/router.js
+++ src/router.js
@@ -2,11 +2,19 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
     Route,
-} from 'react-router-dom';
+}
+ from 'react-router-dom';
 import App from './App';
+import Index from './pages/Index'
+import Show from './pages/Show'
+
 
 const router = createBrowserRouter(
-    createRoutesFromElements(<Route path='/' element={<App />}></Route>)
+    createRoutesFromElements(
+    <Route path='/' element={<App />}>
+        <Route path = '' element={<Index />}/>
+        <Route path = '/:id' element={<Show/>}/>
+    </Route>)
 );
 
 export default router
