36a8e5f70e0a0b49bf35614c8062ddcf7d4fd01c
Alec Hannaford <alec322@gmail.com>
2023-01-17 17:39:28 -0800

    added action and method to form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,9 +4,9 @@ function Index(props){
     return (
     <div>
         <h1>Create a new Bookmark</h1>
-        <Form>
-            <input type='text'/>
-            <input type='text'/>
+        <Form action = '/create' method='post'>
+            <input type='text' name ='title'/>
+            <input type='text' name = 'url'/>
             <input type='submit'/>
         </Form>
         <h1>INDEX COMPONENT</h1>
