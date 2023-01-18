6aa4fe1fa2d542d553f517aa3d0784bac9f5a584
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2023-01-17 18:04:11 -0800

    updated index to use loader data

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,16 +1,28 @@
-import {Form} from 'react-router-dom'
+import { Link, useLoaderData, Form} from 'react-router-dom'
 
 function Index(props){
+    const bookmarks = useLoaderData()
+
     return (
-    <div>
+        <div>
+            <h1>Bookmarked Pages</h1>
+        {bookmarks.map((bookmark) => (
+            <div key={bookmark._id}>
+                <a href={bookmark.url}>{bookmark.title}</a> 
+            </div>
+        ))}
+
+    
         <h1>Create a new Bookmark</h1>
         <Form>
             <input type='text'/>
             <input type='text'/>
             <input type='submit'/>
         </Form>
-        <h1>INDEX COMPONENT</h1>
-    </div>
+        </div>
+
+
+
     )
 }
 
