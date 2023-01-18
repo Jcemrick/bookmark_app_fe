6b067365a1bd531396b0e2716b163f876a507c89
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2023-01-18 15:40:53 -0800

    Updated Index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,18 @@
-import { Link, useLoaderData, Form} from 'react-router-dom'
+import { Link, useLoaderData, Form, useState} from 'react-router-dom'
 
 function Index(props){
+    const [titleState, setTitleState] = useState('')
+    const [urlState, setUrlState] = useState('')
+    function handleSubmit(e){
+        console.log("SUBMITT")
+        setTitleState('')
+        setUrlState('')
+    }
+
     const bookmarks = useLoaderData()
 
     return (
-        <div>
+    <div>
             <h1>Bookmarked Pages</h1>
         {bookmarks.map((bookmark) => (
             <div key={bookmark._id}>
@@ -12,18 +20,26 @@ function Index(props){
             </div>
         ))}
 
-    
         <h1>Create a new Bookmark</h1>
-        <Form>
-            <input type='text'/>
-            <input type='text'/>
+        <Form onSubmit={handleSubmit} action = '/create' method='post'>
+            <input 
+            type='text' 
+            name ='title'
+            placeholder='Title' 
+            value={titleState} 
+            onChange = {event => setTitleState(event.target.value)} 
+            />
+
+            <input 
+            type='text' 
+            name = 'url'
+            placeholder='URL'
+            value={urlState}
+            onChange = {event => setUrlState(event.target.value)}
+            />
             <input type='submit'/>
         </Form>
-        </div>
-
-
-
+        <h1>INDEX COMPONENT</h1>
+    </div>
     )
 }
-
-export default Index
