5d7e8d26a36eb267a4972f4734ea09b2005d4311
abethel912 <abethel180@gmail.com>
2023-01-17 20:14:53 -0500

    Update and Delete routes created with actions

---
+++ src/actions.js
@@ -0,0 +1,27 @@
+import { redirect } from 'react-router-dom'
+
+const URL = 'https://bookmark-app-pli9.onrender.com/'
+
+export const deleteAction = async ({ params }) => {
+  await fetch(URL + params.id, {
+    method: 'delete'
+  })
+  return redirect('/')
+}
+
+export const updateAction = async ({ request, params }) => {
+  const formData = await request.formData()
+  const updatedBookmark = {
+    name: formData.get('name'),
+    url: formData.get('url')
+  }
+
+  await fetch(URL + params.id, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedBookmark)
+  })
+  return redirect('/')
+}
--- src/router.js
+++ src/router.js
@@ -10,12 +10,15 @@ import Show from './pages/Show'
 
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-    <Route path='/' element={<App />}>
-        <Route path = '' element={<Index />}/>
-        <Route path = '/:id' element={<Show/>}/>
-        <Route path = 'create'/>
-    </Route>)
-);
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Index />} />
+      <Route path="/:id" element={<Show />} />
+      <Route path="create" />
+      <Route path="update/:id" />
+      <Route path="delete/:id" />
+    </Route>
+  )
+)
 
 export default router
