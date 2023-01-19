d77d4811e31b7f361aca7a00603361cd16bd0f50
Joe Emrick <98963829+Jcemrick@users.noreply.github.com>
2023-01-19 09:30:24 -0800

    Update loaders.js

--- src/loaders.js
+++ src/loaders.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:4000';
+const URL = 'https://bookmark-app-fe.onrender.com';
 
 export const bookmarkLoader = async () => {
     const response = await fetch(URL + '/bookmark')
